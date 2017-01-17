beeb5b51cf839d7a569e5849c3aca1ff6cf14235
lisongwei1593 <lisongwei1593@gmail.com>
2017-01-17 10:38:06 +0800

    UPD dingyunpeng

---
+++ ijiapeng/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ijiapeng',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
